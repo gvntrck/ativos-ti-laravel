d2d90bf872c89f07b2d020294d2026fa12301521
giovani <gvntrck@gmail.com>
2026-02-01 18:22:55 -0300

    ajusta footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="mt-12 pt-6 border-t border-slate-200 text-center">
     <p class="text-sm text-slate-500">
-        &copy; <?php echo date('Y'); ?> projetoalfa.org
+        projetoalfa.org
         <span class="mx-1">|</span>
         Versão <?php echo ComputerControlSystem::VERSION; ?>
     </p>
