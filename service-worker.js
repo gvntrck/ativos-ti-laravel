6c058e8d753d14c178a7ba1255a96690d2a1b9d5
giovani <gvntrck@gmail.com>
2026-02-02 22:44:30 -0300

    ajusta filtros no mobile

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'controlepcs-v1';
+const CACHE_NAME = 'controlepcs-v2';
 const urlsToCache = [
     './',
     './index.php',
@@ -13,6 +13,21 @@ self.addEventListener('install', event => {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
