a5942d8363e7a282bc0f1fb7f08c89835c3ba4ef
giovani <gvntrck@gmail.com>
2026-02-12 10:46:22 -0300

    ajuste de alinhamento à direita em campos de texto longo com largura total da célula

--- templates/view-reports.php
+++ templates/view-reports.php
@@ -179,8 +179,9 @@ foreach ($report_columns as $column) {
                             $raw_value = isset($row->$column) ? (string) $row->$column : '';
                             $formatted_value = $format_report_value($column, $raw_value);
                             $is_long_text = in_array($column, ['specs', 'notes'], true);
+                            $is_specs_or_notes = $is_long_text;
                             ?>
-                            <td class="px-3 py-2 align-top">
+                            <td class="px-3 py-2 align-top <?php echo $is_specs_or_notes ? 'text-right' : ''; ?>">
                                 <?php if ($column === 'hostname' && $row_id > 0): ?>
                                     <a href="?view=details&id=<?php echo $row_id; ?>"
                                         class="text-indigo-600 hover:text-indigo-900 font-medium">
@@ -193,7 +194,7 @@ foreach ($report_columns as $column) {
                                     </a>
                                 <?php else: ?>
                                     <span
-                                        class="<?php echo $is_long_text ? 'whitespace-pre-wrap break-words text-xs text-slate-700 block text-right' : 'text-slate-700 block whitespace-nowrap overflow-hidden text-ellipsis'; ?>"
+                                        class="<?php echo $is_long_text ? 'whitespace-pre-wrap break-words text-xs text-slate-700 block w-full text-right' : 'text-slate-700 block whitespace-nowrap overflow-hidden text-ellipsis'; ?>"
                                         title="<?php echo esc_attr($formatted_value); ?>">
                                         <?php echo esc_html($formatted_value); ?>
                                     </span>
